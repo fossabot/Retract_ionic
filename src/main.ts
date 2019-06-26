ef22621ab49f9f7450bd5cff18a7c400383a9b0f
Shreyansh Nayak <nayakshreyansh14@gmail.com>
2019-06-26 18:12:40 +0530

    Initial Setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
